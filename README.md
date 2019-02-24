dad91ac1edf2d8d89e6ebc94040dae3d5841a200
Lisa-Calderaro <lisacalderaro@gmail.com>
2019-02-23 18:29:21 -0800

    created game code javascript, html, jQuery

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # game-html
+This is an html game for beginners. It will evolve into a more elaborate final project. 
---
+++ game1.js
@@ -0,0 +1,17 @@
+<html>
+    <head>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <style>
+            
+        </style>
+
+        <script>
+            jQuery(document).ready(function(){     
+            );
+        </script>
+    </head>
+    <body>
+        <div class="">
+        </div>
+    </body>
+</html>
